0e3a2f52017ea3e4c31253942274e20e4dc030d6
ducca <chuanhduc09@gmail.com>
2025-10-22 02:12:27 +0000

    add MFH

--- C2KD/ave/main_overlap_tag1.py
+++ C2KD/ave/main_overlap_tag1.py
@@ -4,7 +4,7 @@ import torch
 import argparse
 import numpy as np
 import random
-from utils.helper import gen_data, train_network_distill, train_network_distill2, train_network_distill3, pre_train
+from utils.helper import gen_data, train_network_distill4, pre_train
 # from utils.model import ImageNet, AudioNet
 from utils.model_res import ImageNet, AudioNet
 from utils.module import Tea, Stu
@@ -28,12 +28,12 @@ def eval_overlap_tag(loader, device, args):
     tea_model.load_state_dict(
         #change
         torch.load('./results/teacher_mod_' + str(tea_type) + '_' + arch + '_' + str(args.num_frame) + '_overlap.pkl',
-                   map_location={"cuda:0": "cpu"}), strict=False)
+                   map_location={"cuda:0": "cuda:1"}), strict=False)
     print(f'Finish Loading teacher model')
 
     net = ImageNet(args).to(device) if stu_type == 0 else AudioNet(args).to(device)
-    tea = Tea().cuda()
-    stu = Stu().cuda()
+    tea = Tea().to(device)
+    stu = Stu().to(device)
     optimizer = torch.optim.SGD([
             {'params': net.parameters()},
             {'params': tea_model.parameters()},
@@ -41,9 +41,7 @@ def eval_overlap_tag(loader, device, args):
             {'params': stu.parameters()},
         ], lr=args.lr, momentum=0.9)
 
-    net.fc
-
-    acc = train_network_distill3(stu_type, tea_model, args.num_epochs, loader, net, device, optimizer, args, tea, stu)
+    acc = train_network_distill4(stu_type, tea_model, args.num_epochs, loader, net, device, optimizer, args, tea, stu)
     return acc
 
 
@@ -52,7 +50,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # the parameters you might need to change
-    parser.add_argument('--gpu', type=int, default=0, help='gpu id')
+    parser.add_argument('--gpu', type=int, default=1, help='gpu id')
     parser.add_argument('--stu-type', type=int, default=0, help='the modality of student unimodal network, 0 for image, 1 for audio')
     parser.add_argument('--num-runs', type=int, default=1, help='num runs')
     parser.add_argument('--num-epochs', type=int, default=100, help='num epochs')
