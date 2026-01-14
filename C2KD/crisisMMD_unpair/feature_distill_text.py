96e01c5b085aad9c3b702517d12528f47db43a2b
khiembk <trainkhiem2003@gmail.com>
2026-01-14 19:02:00 +0700

    completeFeatureDistill

--- C2KD/crisisMMD_unpair/feature_distill_text.py
+++ C2KD/crisisMMD_unpair/feature_distill_text.py
@@ -11,7 +11,7 @@ from transformers import AutoModelForSequenceClassification
 from train_mobilenet_teacher import get_pretraining_techer_model
 from torchvision import transforms
 from torch.utils.data import DataLoader
-
+import ot
 
 def count_trainable_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -37,6 +37,24 @@ HUMANITARIAN_LABEL2ID = {
 }
 HUMANITARIAN_ID2LABEL = {v: k for k, v in HUMANITARIAN_LABEL2ID.items()}
 
+def compute_FA(stu_f, tea_f, device):
+    batch_size = stu_f.size(0)
+    a = torch.ones(batch_size, device=device) / batch_size
+    b = torch.ones(batch_size, device=device) / batch_size
+
+    eps = 1e-8
+    stu_f_norm = stu_f / (stu_f.norm(dim=1, keepdim=True) + eps)
+    tea_f_norm = tea_f / (tea_f.norm(dim=1, keepdim=True) + eps)
+
+    M = 1.0 - torch.matmul(stu_f_norm, tea_f_norm.T)
+    M = torch.clamp(M, min=0.0)
+
+    fa = ot.sinkhorn2(
+            a, b, M,
+            reg=0.1,
+            numItermax=100
+        )
+    return fa
 
 def feature_distill_one_epoch(student_model, teacher_model, projector ,data_loader,  optimizer,  device, lambda_fa=1.0):
     student_model.train()
@@ -49,6 +67,43 @@ def feature_distill_one_epoch(student_model, teacher_model, projector ,data_load
     total_loss = 0.0
     correct = 0
     
+    for batch in data_loader:
+        optimizer.zero_grad()
+        input_ids = batch["input_ids"].to(device)
+        attention_mask = batch["attention_mask"].to(device)
+        images = batch["image"].to(device)
+        labels = batch["label"].to(device)
+        
+        with torch.no_grad():
+            _, tea_f = teacher_model(images, return_feature = True) 
+
+        
+        stu_logits, stu_f = student_model(input_ids, attention_mask, return_feature = True)
+        ### adding projector
+        stu_f = projector(stu_f)
+
+        CE_loss = criterion(stu_logits, labels)
+        FA_loss = compute_FA(stu_f, tea_f,device)
+        
+        loss = CE_loss + lambda_fa * FA_loss
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        total_fa += CE_loss.item()
+        total_ce += FA_loss.item()
+
+        preds = torch.argmax(stu_logits, dim=1)
+        correct += (preds == labels).sum().item()
+        total += labels.size(0)
+
+    return {
+        "loss": total_loss / len(data_loader),
+        "ce": total_ce / len(data_loader),
+        "fa": total_fa / len(data_loader),
+        "acc": correct / total
+    }
+
 
 def train():
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
@@ -84,6 +139,11 @@ def train():
         in_dim= student_model.feature_dim,      # student hidden
         out_dim=teacher_model.feature_dim  # define in teacher
     ).to(device)
+    
+    optimizer = torch.optim.AdamW(
+        list(student_model.parameters()) + list(projector.parameters()),
+        lr=3e-4
+    )
 
 
 
